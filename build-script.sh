9ec11b720aad229f51f1e2462654966bda60cd9a
Delphine COILLE <46934712+dcoille@users.noreply.github.com>
2020-10-01 17:23:57 +0200

    Update for version 7.10.6
    Update release Bonita version with 7.10.6

--- build-script.sh
+++ build-script.sh
@@ -17,7 +17,7 @@ BONITA_BUILD_STUDIO_ONLY=${BONITA_BUILD_STUDIO_ONLY:-false}
 BONITA_BUILD_STUDIO_SKIP=${BONITA_BUILD_STUDIO_SKIP:-false}
 
 # Bonita version
-BONITA_VERSION=7.10.5
+BONITA_VERSION=7.10.6
 
 
 ########################################################################################################################
